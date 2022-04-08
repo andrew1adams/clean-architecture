bc0a2fb035872332b753b88659ae361fb978061d
Andrew Adams <andreewb02@gmail.com>
2022-04-08 01:46:16 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Login } from '@/presentation/pages';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const SystemUnderTestCreator = (): SutTypes => {
+  const sut = render(<Login />);
+
+  return {
+    sut,
+  };
+};
 
 describe('Login', () => {
   it('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />);
+    const { sut } = SystemUnderTestCreator();
 
-    const errorWrapper = getByTestId('error-wrapper');
+    const errorWrapper = sut.getByTestId('error-wrapper');
     expect(errorWrapper.childElementCount).toBe(0);
 
-    const submitBtn = getByTestId('submit-btn') as HTMLButtonElement;
+    const submitBtn = sut.getByTestId('submit-btn') as HTMLButtonElement;
     expect(submitBtn.disabled).toBe(true);
 
-    const emailStatus = getByTestId('email-status');
+    const emailStatus = sut.getByTestId('email-status');
     expect(emailStatus.title).toBe('Required Field');
     expect(emailStatus.className).toContain('error');
 
-    const passwordStatus = getByTestId('password-status');
+    const passwordStatus = sut.getByTestId('password-status');
     expect(passwordStatus.title).toBe('Required Field');
     expect(passwordStatus.className).toContain('error');
   });
