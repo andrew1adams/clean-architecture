d22c0ac71de54e24a38e833c993b0c87053050c7
Andrew Adams <andreewb02@gmail.com>
2022-04-10 01:55:42 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,11 +1,18 @@
 import { InvalidFieldError } from '@/validation/errors';
 import { MinLengthValidation } from '@/validation/validators';
+import faker from 'faker';
 
 describe('Min Length Validation', () => {
   test('Should return error if value is invalid', () => {
     const sut = new MinLengthValidation('field', 5);
     const error = sut.validate('123');
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5);
+    const error = sut.validate('123456');
+    expect(error).toBeFalsy();
   });
 });
 
