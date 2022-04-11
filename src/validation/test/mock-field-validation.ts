0cf2f21e514bd46d4f13399605bc04c0888bc4eb
Andrew Adams <andreewb02@gmail.com>
2022-04-11 02:07:25 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/test/mock-field-validation.ts
@@ -0,0 +1,17 @@
+import { FieldValidation } from '@/validation/protocols';
+
+class FieldValidationSpy implements FieldValidation {
+  readonly field: string;
+  error: Error = null;
+
+  constructor(field: string) {
+    this.field = field;
+  }
+
+  validate(value: string): Error {
+    return this.error;
+  }
+}
+
+export { FieldValidationSpy };
+
