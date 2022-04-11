579041dc2e212577089d5163531d1e55158ffe91
Andrew Adams <andreewb02@gmail.com>
2022-04-11 02:21:55 -0300

    test: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 import { FieldValidationSpy } from '@/validation/test';
 import { ValidationComposite } from '@/validation/validators';
+import faker from 'faker';
 
 type SutTypes = {
   sut: ValidationComposite;
   fieldValidationsSpy: FieldValidationSpy[];
 };
 
-const SystemUnderTestCreator = (): SutTypes => {
+const SystemUnderTestCreator = (field: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy(field),
+    new FieldValidationSpy(field),
   ];
 
   const sut = new ValidationComposite(fieldValidationsSpy);
@@ -22,15 +23,25 @@ const SystemUnderTestCreator = (): SutTypes => {
 
 describe('Validation Composite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = SystemUnderTestCreator();
+    const field = faker.random.word();
+    const { sut, fieldValidationsSpy } = SystemUnderTestCreator(field);
+
+    const errors = [faker.random.words(), faker.random.words()];
 
     fieldValidationsSpy.forEach(
-      (fieldValidation, idx) =>
-        (fieldValidation.error = new Error(`error_message_${idx}`))
+      (fieldValidation, idx) => (fieldValidation.error = new Error(errors[idx]))
     );
 
-    const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe('error_message_0');
+    const error = sut.validate(field, faker.random.word());
+    expect(error).toBe(errors[0]);
+  });
+
+  test('Should return falsy if all validations pass', () => {
+    const field = faker.random.word();
+    const { sut } = SystemUnderTestCreator(field);
+
+    const error = sut.validate(field, faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
 
