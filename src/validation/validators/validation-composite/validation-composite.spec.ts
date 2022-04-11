dc34327e733a7a2b5e2d6a4d20205631c2a6830f
Andrew Adams <andreewb02@gmail.com>
2022-04-11 02:09:12 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,14 +4,15 @@ import { ValidationComposite } from '@/validation/validators';
 describe('Validation Composite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field');
+    fieldValidationSpy.error = new Error('first_error');
     const fieldValidationSpy2 = new FieldValidationSpy('any_field');
-    fieldValidationSpy2.error = new Error('any_message');
+    fieldValidationSpy2.error = new Error('second_error');
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2,
     ]);
     const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe('any_message');
+    expect(error).toBe('first_error');
   });
 });
 
