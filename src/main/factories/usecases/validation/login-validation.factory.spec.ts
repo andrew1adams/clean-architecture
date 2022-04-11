ef985c529e79c396ad3cdf6528843c56aa7ca4ca
Andrew Adams <andreewb02@gmail.com>
2022-04-11 03:28:21 -0300

    test: ensure LoginValidation use correct validations

---
+++ src/main/factories/usecases/validation/login-validation.factory.spec.ts
@@ -0,0 +1,18 @@
+import { LoginValidationCreator } from '@/main/factories';
+import {
+  ValidationBuilder,
+  ValidationComposite,
+} from '@/validation/validators';
+
+describe('Login Validation Factory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = LoginValidationCreator();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+      ])
+    );
+  });
+});
+
