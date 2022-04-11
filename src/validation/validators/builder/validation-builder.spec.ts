4ccf78c20db090cfa7ceb9c7e79d4895aee2e458
Andrew Adams <andreewb02@gmail.com>
2022-04-11 02:45:10 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,6 +1,7 @@
 import {
   RequiredFieldValidation,
   ValidationBuilder,
+  EmailValidation,
 } from '@/validation/validators';
 
 describe('Validation Builder', () => {
@@ -8,6 +9,12 @@ describe('Validation Builder', () => {
     const validations = ValidationBuilder.field('any_field').required().build();
 
     expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+  });
+
+  test('Should return EmailValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build();
+
+    expect(validations).toEqual([new EmailValidation('any_field')]);
   });
 });
 
