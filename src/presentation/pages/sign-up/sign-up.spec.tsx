3291ea2b0f47d1397af4395cdad413db774809bd
Andrew Adams <andreewb02@gmail.com>
2022-04-17 19:44:06 -0300

    test: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/sign-up/sign-up.spec.tsx
+++ src/presentation/pages/sign-up/sign-up.spec.tsx
@@ -1,30 +1,63 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import { SignUp } from '@/presentation/pages'
-import { testChildCount, testButtonIsDisabled, testStatusField } from '@/presentation/test'
+import {
+  testChildCount,
+  testButtonIsDisabled,
+  testStatusField,
+  ValidationStub
+} from '@/presentation/test'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RenderResult
 }
 
-const SystemUnderTestCreator = (): SutTypes => {
-  const sut = render(<SignUp />)
+type SutParams = {
+  validationError: string
+}
+
+const SystemUnderTestCreator = (params?: SutParams): SutTypes => {
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError
+
+  const sut = render(<SignUp validation={validationStub} />)
 
   return {
     sut
   }
 }
 
+const populateField = (
+  sut: RenderResult,
+  fieldName: string,
+  value: any = faker.random.word()
+): void => {
+  const element = sut.getByTestId(`${fieldName}-input`)
+  fireEvent.input(element, { target: { value } })
+}
+
 describe('Login', () => {
+  afterEach(cleanup)
+
   test('Should start with initial state', () => {
-    const validationError = 'Filled in Correctly'
-    const { sut } = SystemUnderTestCreator()
+    const validationError = faker.random.words()
+    const fixedValue = 'Filled in Correctly'
+    const { sut } = SystemUnderTestCreator({ validationError })
 
     testChildCount(sut, 'error-wrapper', 0)
     testButtonIsDisabled(sut, 'submit-btn', true)
     testStatusField(sut, 'name', validationError)
-    testStatusField(sut, 'email', validationError)
-    testStatusField(sut, 'password', validationError)
-    testStatusField(sut, 'passwordConfirmation', validationError)
+    testStatusField(sut, 'email', fixedValue)
+    testStatusField(sut, 'password', fixedValue)
+    testStatusField(sut, 'passwordConfirmation', fixedValue)
+  })
+
+  test('Should show name error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = SystemUnderTestCreator({ validationError })
+
+    populateField(sut, 'name')
+    testStatusField(sut, 'name', validationError)
   })
 })
