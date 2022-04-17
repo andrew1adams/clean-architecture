b83f47b2c6ce56d880876536dc002950924ce74f
Andrew Adams <andreewb02@gmail.com>
2022-04-17 14:18:05 -0300

    feat: add SignUp view

---
+++ src/presentation/pages/sign-up/sign-up.tsx
@@ -0,0 +1,39 @@
+import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
+import { LoginFormContext } from '@/presentation/contexts'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './sign-up.module.scss'
+
+const { signUp, form, submit, link } = styles
+
+const SignUp: React.FC = () => {
+  return (
+    <div className={signUp}>
+      <LoginHeader />
+      <LoginFormContext.Provider value={{ state: {} }}>
+        <form className={form}>
+          <h2>Register</h2>
+          <Input name='name' type='text' placeholder='Insert your name' />
+          <Input name='email' type='email' placeholder='Insert your email' />
+          <Input type='password' name='password' placeholder='Insert your password' />
+          <Input
+            type='password'
+            name='passwordConfirmation'
+            placeholder='Insert your password again'
+          />
+
+          <button className={submit} type='submit'>
+            Sign In
+          </button>
+          <Link to='/login' className={link}>
+            sign in
+          </Link>
+          <FormStatus />
+        </form>
+      </LoginFormContext.Provider>
+      <Footer />
+    </div>
+  )
+}
+
+export { SignUp }
--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SignUp } from '@/presentation/pages'
 import '@/presentation/styles/global.module.scss'
 
 type MainRouterProps = {
@@ -10,6 +11,7 @@ const Router: React.FC<MainRouterProps> = ({ login }: MainRouterProps) => (
   <BrowserRouter>
     <Routes>
       <Route path='/login' element={login} />
+      <Route path='/sign-up' element={<SignUp />} />
     </Routes>
   </BrowserRouter>
 )
