3bb6f59ab45f3e9e3fe550d36ef19064c480ae96
Andrew Adams <andreewb02@gmail.com>
2022-04-17 23:41:02 -0300

    test: add SignUpValidation to a factory method

---
+++ src/main/factories/usecases/validation/sign-up-validation/sign-up-validation.factory.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpValidationCreator } from '@/main/factories'
+import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+
+describe('Sign Up Validation Factory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = SignUpValidationCreator()
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('name').required().min(3).build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation').required().min(5).build()
+      ])
+    )
+  })
+})
