90e2eb1d7837725efa0e5229322331e5f13a02bb
Andrew Adams <andreewb02@gmail.com>
2022-04-19 01:57:44 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('/login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Required Field')
+      .should('have.css', 'background-color', 'rgb(255, 82, 82)')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Required Field')
+      .should('have.css', 'background-color', 'rgb(255, 82, 82)')
+    cy.getByTestId('submit-btn').should('be.disabled')
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
   })
 })
