2beb8b8dfad4ed3daeb5894096fd97c6ee651b01
Andrew Adams <andreewb02@gmail.com>
2022-04-19 02:32:25 -0300

    test: ensure Login present error message if InvalidCredentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import * as faker from 'faker'
 
+const baseURL = (url?: string): string => `${Cypress.config('baseUrl')}${url}`
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('/login')
@@ -42,5 +44,22 @@ describe('Login', () => {
       .should('have.css', 'background-color', 'rgb(0, 200, 83)')
     cy.getByTestId('submit-btn').should('not.be.disabled')
     cy.getByTestId('error-wrapper').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email-input').focus().type(faker.internet.email())
+    cy.getByTestId('password-input').focus().type(faker.internet.password())
+    cy.getByTestId('submit-btn').click()
+    cy.getByTestId('error-wrapper')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('exist')
+      .should('contain.text', 'Invalid Credentials')
+    cy.url().should('eq', baseURL('/login'))
   })
 })
