9adedfdaad52f2e0aab8a1520d5f251e5234b167
Andrew Adams <andreewb02@gmail.com>
2022-04-20 03:20:11 -0300

    test: ensure throws UnexpectedError on 200 if invalid data is returned

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -85,4 +85,26 @@ describe('Login', () => {
       .should('contain.text', 'Something was wrong, try again later.')
     cy.url().should('eq', baseURL('/login'))
   })
+
+  it('Should present UnexpectedError if invalid data is returned', () => {
+    const randomProperty = faker.random.word()
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        [randomProperty]: faker.random.uuid()
+      }
+    }).as('login')
+    cy.getByTestId('email-input').focus().type('mango@gmail.com')
+    cy.getByTestId('password-input').focus().type('12345')
+    cy.getByTestId('submit-btn').click()
+    cy.wait('@login').then(XMLHttpRequest => {
+      expect(XMLHttpRequest.response.statusCode).to.eq(200)
+      expect(XMLHttpRequest.response.body).haveOwnProperty(randomProperty)
+    })
+    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('main-error')
+      .should('exist')
+      .should('contain.text', 'Something was wrong, try again later.')
+    cy.url().should('eq', baseURL('/login'))
+  })
 })
