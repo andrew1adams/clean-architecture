b472e71f0234724bff7d75eed98cf3bed052c9b6
Andrew Adams <andreewb02@gmail.com>
2022-04-24 02:19:47 -0300

    refactor: move Icon to its own file

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,13 +1,10 @@
-import { Footer, Header } from '@/presentation/components'
+import { Footer, Header, Icon } from '@/presentation/components'
 import React from 'react'
 import styles from './survey-list.module.scss'
 
-const { surveyListWrapper, contentWrapper, surveyContent, day, month, year, iconWrapper, red } =
-  styles
+const { surveyListWrapper, contentWrapper, surveyContent, day, month, year, iconWrapper } = styles
 
 const SurveyList: React.FC = () => {
-  const getStatus = (): string => `${red}`
-
   return (
     <div className={surveyListWrapper}>
       <Header />
@@ -16,12 +13,7 @@ const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className={surveyContent}>
-              <div className={`${iconWrapper} ${getStatus()}`}>
-                <img
-                  src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII='
-                  alt='Icon'
-                />
-              </div>
+              <Icon iconName='thumbDown' className={iconWrapper} />
               <time>
                 <span className={day}>20</span>
                 <span className={month}>04</span>
