ad2e27e8fa8e2ee1d34b9ab34671b9412d953760
Andrew Adams <andreewb02@gmail.com>
2022-04-25 19:38:46 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 
+const SystemUnderTestCreator = (): void => {
+  render(<SurveyList />)
+}
+
 describe('SurveyListcomponent', () => {
   test('Should present 6 empty items on start', () => {
-    render(<SurveyList />)
+    SystemUnderTestCreator()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(6)
   })
