698f931311446b5be1b6ac7de9db1235e998886c
Andrew Adams <andreewb02@gmail.com>
2022-04-25 19:53:15 -0300

    test: ensure SurveyList calls LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
-const SystemUnderTestCreator = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+
+  async load(): Promise<SurveyModel[]> {
+    this.callsCount++
+
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const SystemUnderTestCreator = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyListcomponent', () => {
@@ -11,5 +32,10 @@ describe('SurveyListcomponent', () => {
     SystemUnderTestCreator()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(6)
+  })
+
+  test('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = SystemUnderTestCreator()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
