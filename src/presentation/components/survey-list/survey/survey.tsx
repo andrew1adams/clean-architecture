06473d8a822c3620abf7cdba39cf41ead6f7ed42
Andrew Adams <andreewb02@gmail.com>
2022-04-25 20:34:50 -0300

    feat: ensure Survey load icon and day correctly

--- src/presentation/components/survey-list/survey/survey.tsx
+++ src/presentation/components/survey-list/survey/survey.tsx
@@ -10,13 +10,15 @@ type Props = {
 }
 
 const Survey: React.FC<Props> = ({ survey }: Props) => {
+  const getIcon = (): 'thumbUp' | 'thumbDown' => (survey.didAnswer ? 'thumbUp' : 'thumbDown')
+
   return (
     <li className={surveyWrapper}>
       <div className={surveyContent}>
-        <Icon iconName='thumbUp' className={iconWrapper} />
+        <Icon iconName={getIcon()} className={iconWrapper} />
         <time>
           <span className={day} data-testid='survey-day'>
-            {survey.date.getDate()}
+            {survey.date.getDate().toString().padStart(2, '0')}
           </span>
           <span className={month} data-testid='survey-month'>
             {survey.date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}
