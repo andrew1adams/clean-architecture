d2496b06033d8f19ccd7dfe5e12d567fe8f795f0
Andrew Adams <andreewb02@gmail.com>
2022-04-28 01:04:29 -0300

    feat: ensure SurveyList renders Surveys on success

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,5 +1,6 @@
+import { SurveyModel } from '@/domain/models'
 import { LoadSurveyList } from '@/domain/usecases'
-import { Footer, Header, SurveySkeleton } from '@/presentation/components'
+import { Footer, Header, Survey, SurveySkeleton } from '@/presentation/components'
 import React, { useEffect } from 'react'
 import styles from './survey-list.module.scss'
 
@@ -9,18 +10,32 @@ type Props = {
   loadSurveyList: LoadSurveyList
 }
 
+type StateProps = {
+  surveys: SurveyModel[]
+}
+
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = React.useState<StateProps>({
+    surveys: []
+  })
+
   useEffect(() => {
-    ;(async () => await loadSurveyList.load())()
+    loadSurveyList.load().then(surveys => setState({ surveys }))
   }, [])
 
   return (
-    <div className={surveyListWrapper} data-testid='survey-list'>
+    <div className={surveyListWrapper}>
       <Header />
       <div className={contentWrapper}>
         <h2>Enquetes</h2>
-        <ul>
-          <SurveySkeleton />
+        <ul data-testid='survey-list'>
+          {state.surveys.length
+            ? (
+                state.surveys.map((survey: SurveyModel) => <Survey survey={survey} key={survey.id} />)
+              )
+            : (
+            <SurveySkeleton />
+              )}
         </ul>
       </div>
       <Footer />
