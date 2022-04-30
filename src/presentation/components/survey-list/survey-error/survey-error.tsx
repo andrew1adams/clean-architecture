f2de726b99e4de4752201bdb7333e6af5b941d18
Andrew Adams <andreewb02@gmail.com>
2022-04-30 00:17:54 -0300

    feat: ensure SurveyList calls LoadSurveyList on reload

--- src/presentation/components/survey-list/survey-error/survey-error.tsx
+++ src/presentation/components/survey-list/survey-error/survey-error.tsx
@@ -3,12 +3,22 @@ import React, { useContext } from 'react'
 import { SurveyContext } from '@/presentation/contexts'
 
 const SurveyError: React.FC = () => {
-  const { state } = useContext(SurveyContext)
+  const { state, setState } = useContext(SurveyContext)
+
+  const handleReload = (): void => {
+    setState({
+      surveys: [],
+      error: '',
+      isReload: !state.isReload
+    })
+  }
 
   return (
     <div>
       <span data-testid='error-wrapper'>{state.error}</span>
-      <button>Recarregar</button>
+      <button data-testid='reload-btn' onClick={handleReload}>
+        Tentar novamente
+      </button>
     </div>
   )
 }
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -16,12 +16,14 @@ type Props = {
 type StateProps = {
   surveys: SurveyModel[]
   error: string
+  isReload: boolean
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const [state, setState] = React.useState<StateProps>({
     surveys: [],
-    error: ''
+    error: '',
+    isReload: false
   })
 
   useEffect(() => {
@@ -29,7 +31,7 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       .load()
       .then(surveys => setState({ ...state, surveys }))
       .catch(err => setState({ ...state, error: err.message }))
-  }, [])
+  }, [state.isReload])
 
   return (
     <div className={surveyListWrapper}>
