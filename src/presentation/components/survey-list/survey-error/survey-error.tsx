0704f6a7c903e55adc21fde6a66add9f8777a457
Andrew Adams <andreewb02@gmail.com>
2022-04-30 00:41:10 -0300

    feat: change LoadSurveyList error UI

--- src/presentation/components/survey-list/survey-error/survey-error.tsx
+++ src/presentation/components/survey-list/survey-error/survey-error.tsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react'
 
 import { SurveyContext } from '@/presentation/contexts'
 
+import styles from './survey-error.module.scss'
+
+const { errorWrapper } = styles
+
 const SurveyError: React.FC = () => {
   const { state, setState } = useContext(SurveyContext)
 
@@ -14,7 +18,7 @@ const SurveyError: React.FC = () => {
   }
 
   return (
-    <div>
+    <div className={errorWrapper}>
       <span data-testid='error-wrapper'>{state.error}</span>
       <button data-testid='reload-btn' onClick={handleReload}>
         Tentar novamente
