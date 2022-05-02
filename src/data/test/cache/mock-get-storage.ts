b6d286ea0ce8ac371f10f060b0bb57274e74d28b
Andrew Adams <andreewb02@gmail.com>
2022-05-02 09:29:07 -0300

    test: ensure AuthorizeHttpGetClientDecorator calls GetStorage with correct value

---
+++ src/data/test/cache/mock-get-storage.ts
@@ -0,0 +1,16 @@
+import faker from 'faker'
+
+import { GetStorage } from '@/data/protocols'
+
+class GetStorageSpy implements GetStorage {
+  key: string
+  value = faker.random.objectElement()
+
+  get(key: string): any {
+    this.key = key
+
+    return this.value
+  }
+}
+
+export { GetStorageSpy }
--- src/data/test/index.ts
+++ src/data/test/index.ts
@@ -2,3 +2,5 @@ export * from './http/mock-http-post-client'
 export * from './http/mock-http-post-request'
 export * from './http/mock-http-get-client'
 export * from './http/mock-http-get-request'
+
+export * from './cache/mock-get-storage'
