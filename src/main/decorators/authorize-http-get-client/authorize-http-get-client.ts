3d1324f71be2c404dfbd37470ead1a949d5c1da7
Andrew Adams <andreewb02@gmail.com>
2022-05-02 09:59:34 -0300

    feat: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetclient

--- src/main/decorators/authorize-http-get-client/authorize-http-get-client.ts
+++ src/main/decorators/authorize-http-get-client/authorize-http-get-client.ts
@@ -14,6 +14,7 @@ class AuthorizeHttpGetClientDecorator implements HttpGetClient {
     if (account?.accessToken) {
       Object.assign(params, {
         headers: {
+          ...params.headers,
           'x-access-token': account.accessToken
         }
       })
